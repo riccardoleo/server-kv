ca412b57a0b940696cedab53391ae7a51766e5ab
riccardoleo <leoriccardo24@gmail.com>
2025-06-25 15:55:59 +0200

    improved Compiler.sh

--- Compiler.sh
+++ Compiler.sh
@@ -5,14 +5,16 @@ set -e  # Ferma lo script se un comando fallisce
 echo "Pulizia dei file oggetto..."
 rm -f *.o
 
+CXXFLAGS="-O3 -funroll-loops -mavx -march=native"
+
 echo "Compilazione dei sorgenti C++..."
-g++ -c CppSQLite3.cpp
-g++ -c KVDatabase.cpp
-g++ -c main.cpp
-g++ -c Server.cpp
+g++ $CXXFLAGS -c CppSQLite3.cpp
+g++ $CXXFLAGS -c KVDatabase.cpp
+g++ $CXXFLAGS -c main.cpp
+g++ $CXXFLAGS -c Server.cpp
 
 echo "Compilazione di sqlite3.c con gcc..."
-gcc -c sqlite3.c
+gcc -O3 -funroll-loops -mavx -march=native -c sqlite3.c
 
 echo "Linking finale..."
 g++ CppSQLite3.o KVDatabase.o main.o Server.o sqlite3.o -o main -lpthread -ldl
